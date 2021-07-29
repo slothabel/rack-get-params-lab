c8b5e9fbf13b136912d9bc3f6b2555b46f1569b2
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-07-29 01:36:47 -0400

    commit tree 9abeb7fca3c1dc069bf20a2df64f9ec12aee7dba

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,14 @@
 class Application
 
+#Create a new route called /cart to show the items in your cart.
+
+#Create a new route called /add that takes in a GET param with the key item.
+# This should check to see if that item is in @@items and add it to the cart if it is. 
+#Otherwise it should give an error.
+
+  
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -13,6 +21,23 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+        if @@cart.empty?
+          resp.write "Your cart is empty"
+        else
+            @@cart.each do |item|
+              resp.write "#{item}\n"
+            end
+        end
+
+    elsif req.path.match(/add/)
+          add_value = req.params["item"]
+          if @@items.include?(add_value)
+            @@cart << add_value
+            resp.write "added #{add_value}"
+          else
+            resp.write "We don't have that item"
+          end
     else
       resp.write "Path Not Found"
     end
